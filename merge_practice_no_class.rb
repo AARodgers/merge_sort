4abc098f9526e14fd2de37d9e882b6712cf52aa8
github username <github email address>
2020-08-04 15:33:24 -0600

    completes merge_practice_no_class.rb

--- merge_practice_no_class.rb
+++ merge_practice_no_class.rb
@@ -1,19 +1,29 @@
 def merge_sort(arr)
 
-    if arr.length <= 1
-        return arr
+    return arr if arr.length <= 1
 
     mid = arr.length / 2
     left = arr.take(mid)
     right = arr.drop(mid)
 
-    sorted_left = left.merge_sort
-    sorted_right = right.merge_sort
+    sorted_left = merge_sort(left)
+    sorted_right = merge_sort(right)
+
     merge(sorted_left, sorted_right)
 end
 
 def merge(left, right)
     sorted = []
 
-    
+    until left.empty? || right.empty?
+        if left.first < right.first
+            sorted << left.shift
+        elsif left.first == right.first
+            sorted << left.shift
+            sorted << right.shift
+        elsif right.first < left.first
+            sorted << right.shift
+        end
+    end
+    sorted + left + right
 end
